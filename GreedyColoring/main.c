cb9def3f5e61acc9d91635277afb9fff2c07d5c5
Erez Mizrahi <erezmizra@gmail.com>
2019-12-23 16:51:13 +0200

    bbb

--- GreedyColoring/main.c
+++ GreedyColoring/main.c
@@ -7,8 +7,21 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Intervals {
+    int begin;
+    int end;
+} Interval;
+
+typedef struct IntervalArray {
+    Interval* intervals;
+    int n;
+} IntArray;
 
 void GreedyColoring();
+void printIntervalFamily(IntArray);
+void calculateEdges(Interval);
 
 int main() {
     GreedyColoring();
@@ -16,9 +29,48 @@ int main() {
 }
 
 void GreedyColoring() {
-    int k;
+    int k, b, e;
+    
     printf("Please input k\n");
     scanf("%d", &k);
     
+    // initialize
+    IntArray ints;
+    ints.n = k;
+    ints.intervals = (Interval*) malloc(ints.n * sizeof(int));
     
+    printf("You will now be asked to insert a family of %d intervals:\n", k);
+    for(int i = 0; i < k; i++) {
+        scanf("%d", &b);
+        scanf("%d", &e);
+        
+        // initialize per interval
+        ints.intervals[i].begin = b;
+        ints.intervals[i].end = e;
+        
+        printf("%dth Interval: %d %d", i, b, e);
+    }
+}
+
+void printIntervalFamily(IntArray a) {
+    printf("The Intervals family is:\n");
+    
+    for(int i = 0; i < a.n; i++) {
+        printf("[%d,%d]", a.intervals[i].begin, a.intervals[i].end);
+        
+        if(i != a.n-1)
+            printf(",");
+        else
+            printf("\n");
+    }
+}
+
+void calculateEdges(IntArray a) {
+    int c = 0;
+    
+    for(int i = 0; i < a.n; i++) {
+        for(int j = 0; j < i; j++) {
+            
+        }
+    }
 }

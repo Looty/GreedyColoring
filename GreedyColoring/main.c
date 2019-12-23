2ab6229dc7fda9c0705ae427d574a030437c8cb3
Erez Mizrahi <erezmizra@gmail.com>
2019-12-23 21:49:01 +0200

    ccc

--- GreedyColoring/main.c
+++ GreedyColoring/main.c
@@ -19,16 +19,22 @@ typedef struct IntervalArray {
     int n;
 } IntArray;
 
-void GreedyColoring();
+void GreedyColoring(void);
+int partition(IntArray arr, int low, int high);
+void swap(Interval* a, Interval* b);
+void quickSort(IntArray arr, int low, int high);
+void printInterval(IntArray a, int i);
 void printIntervalFamily(IntArray);
-void calculateEdges(Interval);
+int calculateEdges(IntArray a);
+int maxDegree(IntArray a);
+int maxDegree(IntArray a);
 
 int main() {
     GreedyColoring();
     return 0;
 }
 
-void GreedyColoring() {
+void GreedyColoring(void) {
     int k, b, e;
     
     printf("Please input k\n");
@@ -48,8 +54,15 @@ void GreedyColoring() {
         ints.intervals[i].begin = b;
         ints.intervals[i].end = e;
         
-        printf("%dth Interval: %d %d", i, b, e);
+        printf("%dth Interval: %d %d\n", i, b, e);
     }
+    
+    printf("Sorting...\n");
+    quickSort(ints, 0, ints.n-1);
+    printf("After sort...\n");
+    printIntervalFamily(ints);
+    
+    printf("G Edges = %d\n", calculateEdges(ints));
 }
 
 void printIntervalFamily(IntArray a) {
@@ -65,12 +78,56 @@ void printIntervalFamily(IntArray a) {
     }
 }
 
-void calculateEdges(IntArray a) {
+int partition(IntArray arr, int low, int high) {
+    int pivot = arr.intervals[high].begin; // pivot
+    int i = (low - 1); // Index of smaller element
+    
+    for (int j = low; j <= high - 1; j++)
+    {
+        // If current element is smaller than the pivot
+        if (arr.intervals[j].begin < pivot)
+        {
+            i++; // increment index of smaller element
+            swap(&arr.intervals[i], &arr.intervals[j]);
+        }
+    }
+    swap(&arr.intervals[i + 1], &arr.intervals[high]);
+    return (i + 1);
+}
+
+void quickSort(IntArray arr, int low, int high)
+{
+    if (low < high)
+    {
+        /* pi is partitioning index, arr[p] is now
+        at right place */
+        int pi = partition(arr, low, high);
+  
+        // Separately sort elements before
+        // partition and after partition
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+void swap(Interval* a, Interval* b)
+{
+    Interval t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int calculateEdges(IntArray a) {
     int c = 0;
     
     for(int i = 0; i < a.n; i++) {
-        for(int j = 0; j < i; j++) {
-            
+        for(int j=i+1; j < a.n; j++) {
+            if(a.intervals[i].end >= a.intervals[j].begin) {
+                c++;
+                //printf("EDGE: [%d,%d] -> [%d,%d]\n", a.intervals[i].begin, a.intervals[i].end, a.intervals[j].begin, a.intervals[j].end);
+            }
         }
     }
+
+    return c;
 }

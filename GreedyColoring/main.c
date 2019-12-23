0c58d2e362375085c20e02112e2a2784c6a4b631
Erez Mizrahi <erezmizra@gmail.com>
2019-12-23 16:08:34 +0200

    Initial Commit

---
+++ GreedyColoring/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  GreedyColoring
+//
+//  Created by Erez Mizrahi on 23/04/19.
+//  Copyright © 2019 Erez Mizrahi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

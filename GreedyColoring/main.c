551fa1c34a95599aba86bf192659486d18616001
Erez Mizrahi <erezmizra@gmail.com>
2019-12-30 14:43:28 +0200

    aaa

--- GreedyColoring/main.c
+++ GreedyColoring/main.c
@@ -4,183 +4,182 @@
 //
 //  Created by Erez Mizrahi on 23/04/19.
 //  Copyright © 2019 Erez Mizrahi. All rights reserved.
-//
-
+// aa
+ 
 #include <stdio.h>
 #include <stdlib.h>
-
-#define NIL    -1
-#define ZERO    0
-
+ 
 typedef struct {
     int begin;
     int end;
-    int deg; //NIL
+    int deg;
     int color;
-    struct Interval* neighbors;
 } Interval;
-
+ 
 typedef struct {
     Interval* intervals;
     int n;
-    int edges; // ZERO
-    int minDeg; // ZERO
-    int maxDeg; // ZERO
-    int chromeNum; // ZERO
+    int edges;
+    int minDeg;
+    int maxDeg;
+    int chromeNum;
+    int compEdges;
+    int maxCompDeg;
+    int minCompDeg;
 } IntArray;
-
+ 
 void GreedyColoring(void);
 int partition(IntArray arr, int low, int high);
 void swap(Interval* a, Interval* b);
 void quickSort(IntArray arr, int low, int high);
-void printInterval(IntArray a, int i);
 void printIntervalFamily(IntArray);
-void InitializeGraph(IntArray);
-int calculateEdges(IntArray a);
-
+int getMinColor(int* arr, int n);
+void optionalColoring(IntArray);
+void InitializeGraph(IntArray*);
+ 
 int main() {
     GreedyColoring();
+    system("pause");
     return 0;
 }
-
+ 
 void GreedyColoring(void) {
     int k, b, e;
-    
+ 
     printf("Please input k\n");
+    //scanf_s("%d", &k);
     scanf("%d", &k);
     
-    // initialize
     IntArray ints;
     ints.n = k;
     ints.intervals = (Interval*) malloc(ints.n * sizeof(Interval));
-    ints.minDeg = ints.maxDeg = ints.edges = ints.chromeNum = ZERO;
-    
+    ints.minDeg = ints.maxDeg = ints.edges = ints.compEdges = ints.maxCompDeg = ints.minCompDeg = 0;
+    ints.chromeNum = 1;
+ 
     printf("You will now be asked to insert a family of %d intervals:\n", k);
     for(int i = 0; i < k; i++) {
-        scanf("%d", &b);
-        scanf("%d", &e);
-        
+        printf("%dth Interval: ", i);
+ 
+        scanf("%d", &b); //scanf_s("%d", &b);
+        scanf("%d", &e); //scanf_s("%d", &e);
+ 
         // initialize per interval
         ints.intervals[i].begin = b;
         ints.intervals[i].end = e;
-        ints.intervals[i].deg = ZERO;
-        ints.intervals[i].neighbors = NULL;
-        ints.intervals[i].color = 1;
-        
-        printf("%dth Interval: %d %d\n", i, b, e);
+        ints.intervals[i].deg = 0;
+        ints.intervals[i].color = -1;
     }
-
-    quickSort(ints, 0, ints.n-1);
+ 
+    quickSort(ints, 0, ints.n - 1);
     printIntervalFamily(ints);
-    
-    //printf("G Edges = %d\n", calculateEdges(ints));
+    InitializeGraph(&ints);
+ 
+    printf("G Edges = %d\n", ints.edges);
+    printf("Maximum Degree of G = %d\n", ints.maxDeg);
+    printf("Minimum Degree of G = %d\n", ints.minDeg);
+    printf("Chromatic Number of G = %d\n", ints.chromeNum);
+    printf("G's Complement Edges = %d\n", ints.compEdges);
+    printf("Maximum Degree of G's Complmenet = %d\n", ints.maxCompDeg);
+    printf("Minimum Degree of G's Complmenet = %d\n", ints.minCompDeg);
+    //printf("Optional Coloring: = %d\n", optionalColoring());
 }
-
+ 
 void printIntervalFamily(IntArray a) {
     printf("The Intervals family is:\n");
-    
+ 
     for(int i = 0; i < a.n; i++) {
         printf("[%d,%d]", a.intervals[i].begin, a.intervals[i].end);
-        
-        if(i != a.n-1)
+ 
+        if(i != a.n - 1)
             printf(",");
         else
             printf("\n");
     }
 }
-
+ 
 int partition(IntArray arr, int low, int high) {
     int pivot = arr.intervals[high].begin; // pivot
     int i = (low - 1); // Index of smaller element
-    
-    for (int j = low; j <= high - 1; j++)
-    {
-        // If current element is smaller than the pivot
-        if (arr.intervals[j].begin < pivot)
-        {
-            i++; // increment index of smaller element
+ 
+    for(int j = low; j <= high - 1; j++) {
+        if(arr.intervals[j].begin < pivot) {
+            i++;
             swap(&arr.intervals[i], &arr.intervals[j]);
         }
     }
     swap(&arr.intervals[i + 1], &arr.intervals[high]);
     return (i + 1);
 }
-
-void quickSort(IntArray arr, int low, int high)
-{
-    if (low < high)
-    {
-        /* pi is partitioning index, arr[p] is now
-        at right place */
+ 
+void quickSort(IntArray arr, int low, int high) {
+    if(low < high) {
         int pi = partition(arr, low, high);
-  
-        // Separately sort elements before
-        // partition and after partition
         quickSort(arr, low, pi - 1);
         quickSort(arr, pi + 1, high);
     }
 }
-
+ 
 // [0,5],[1,2],[1,3],[3,4],[3,5],[3,9],[3,10],[8,11]
 // 0 -> 1
 // 1 -> 2
 // 3 -> 4
 // 8 -> 1
-
-void swap(Interval* a, Interval* b)
-{
+ 
+void swap(Interval* a, Interval* b) {
     Interval t = *a;
     *a = *b;
     *b = t;
 }
-
-void InitializeGraph(IntArray a) {
-    int edges = 0;
-    int minDeg = 0;
-    int maxDeg = 0;
-    int currNeigh = 0;
-    
+ 
+int getMinColor(int* arr, int n) {
+    int min = arr[0];
+ 
+    for(int i = 1; i <= n && !arr[i]; i++) {
+        if(arr[i] < min)
+            min = arr[i];
+    }
+ 
+    return min;
+}
+ 
+void InitializeGraph(IntArray* a) {
+    int currChrome = 1;
     int minColor = 1;
-    int chromeNum = 0;
-    
-    for(int i = 0; i < a.n; i++) {
-        for(int j=i+1; j < a.n; j++) {
-            if(a.intervals[i].end >= a.intervals[j].begin) {
-                edges++;
-                currNeigh++;
-
-                minDeg = ++a.intervals[i].deg;
-                ++a.intervals[j].deg;
-                a.intervals[i].color = minColor;
-
-                //a.intervals[i].color++;
-                       
-                a.intervals[i].neighbors = (struct Interval*) malloc(currNeigh * sizeof(Interval));
-                printf("EDGE: [%d,%d] -> [%d,%d]\n", a.intervals[i].begin, a.intervals[i].end, a.intervals[j].begin, a.intervals[j].end);
+    int* colorArr = (int*) calloc(a->n, sizeof(int)); // color array of V
+ 
+    for(int i = 0; i < a->n; i++) {
+        for(int j = i + 1; j < a->n; j++) {
+            if(a->intervals[i].end >= a->intervals[j].begin) {
+                a->edges++;
+                currChrome++;
+ 
+                if(++a->intervals[i].deg < a->minDeg)
+                    a->minDeg = a->intervals[i].deg;
+ 
+                ++a->intervals[j].deg;
+ 
+                if(a->intervals[j].color != -1)
+                    colorArr[a->intervals[j].color-1]++;
+ 
+                printf("EDGE: [%d,%d] -> [%d,%d]\n", a->intervals[i].begin, a->intervals[i].end, a->intervals[j].begin, a->intervals[j].end);
             }
         }
  
-        if(maxDeg < a.intervals[i].deg)
-            maxDeg = a.intervals[i].deg;
-            
-        currNeigh = 0;
-    }
-}
-/*
- typedef struct {
-     int begin; v
-     int end; v
-     int deg; v
-     int color;
-     struct Interval* next;
- } Interval;
+        a->intervals[i].color = getMinColor(colorArr, a->n)+1;
+        for(int i = 0; i < a->n; i++) { colorArr[i] = 0; }
  
- typedef struct {
-     Interval* intervals;
-     int n;
-     int edges; // ZERO
-     int minDeg; // ZERO
-     int maxDeg; // ZERO
-     int chromeNum; // ZERO
- } IntArray;
- */
+        if(currChrome > a->chromeNum)
+            a->chromeNum = currChrome;
+ 
+        if(a->maxDeg < a->intervals[i].deg)
+            a->maxDeg = a->intervals[i].deg;
+ 
+        currChrome = 0;
+    }
+ 
+    a->compEdges = (a->n*(a->n-1)/2) - a->edges;
+    a->maxCompDeg = a->n-1 - a->minDeg;
+    a->minCompDeg = a->n-1 - a->maxDeg;
+ 
+    free(colorArr);
+}

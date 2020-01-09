8e7873b9ca076164f12e82333a26cb1f58d2484f
Erez Mizrahi <erezmizra@gmail.com>
2020-01-09 17:32:48 +0200

    100% need to be tested in VS

--- GreedyColoring/main.c
+++ GreedyColoring/main.c
@@ -33,13 +33,13 @@ int partition(IntArray arr, int low, int high);
 void swap(Interval* a, Interval* b);
 void quickSort(IntArray arr, int low, int high);
 void printIntervalFamily(IntArray);
-int getMinColor(int* arr, int n);
-void optionalColoring(IntArray);
+void optionalColoring(IntArray*);
+void printColors(IntArray*);
 void InitializeGraph(IntArray*);
  
 int main() {
     GreedyColoring();
-    system("pause");
+    //system("pause");
     return 0;
 }
  
@@ -53,8 +53,8 @@ void GreedyColoring(void) {
     IntArray ints;
     ints.n = k;
     ints.intervals = (Interval*) malloc(ints.n * sizeof(Interval));
-    ints.minDeg = ints.maxDeg = ints.edges = ints.compEdges = ints.maxCompDeg = ints.minCompDeg = 0;
-    ints.chromeNum = 1;
+    ints.maxDeg = ints.edges = ints.compEdges = ints.maxCompDeg = ints.minCompDeg = ints.chromeNum = 0;
+    ints.minDeg = -1;
  
     printf("You will now be asked to insert a family of %d intervals:\n", k);
     for(int i = 0; i < k; i++) {
@@ -69,11 +69,12 @@ void GreedyColoring(void) {
         ints.intervals[i].deg = 0;
         ints.intervals[i].color = -1;
     }
- 
-    quickSort(ints, 0, ints.n - 1);
+
     printIntervalFamily(ints);
+    quickSort(ints, 0, ints.n - 1);
     InitializeGraph(&ints);
- 
+    optionalColoring(&ints);
+    
     printf("G Edges = %d\n", ints.edges);
     printf("Maximum Degree of G = %d\n", ints.maxDeg);
     printf("Minimum Degree of G = %d\n", ints.minDeg);
@@ -81,7 +82,9 @@ void GreedyColoring(void) {
     printf("G's Complement Edges = %d\n", ints.compEdges);
     printf("Maximum Degree of G's Complmenet = %d\n", ints.maxCompDeg);
     printf("Minimum Degree of G's Complmenet = %d\n", ints.minCompDeg);
-    //printf("Optional Coloring: = %d\n", optionalColoring());
+    printColors(&ints);
+    
+    free(ints.intervals);
 }
  
 void printIntervalFamily(IntArray a) {
@@ -98,8 +101,8 @@ void printIntervalFamily(IntArray a) {
 }
  
 int partition(IntArray arr, int low, int high) {
-    int pivot = arr.intervals[high].begin; // pivot
-    int i = (low - 1); // Index of smaller element
+    int pivot = arr.intervals[high].begin;
+    int i = (low - 1);
  
     for(int j = low; j <= high - 1; j++) {
         if(arr.intervals[j].begin < pivot) {
@@ -107,6 +110,7 @@ int partition(IntArray arr, int low, int high) {
             swap(&arr.intervals[i], &arr.intervals[j]);
         }
     }
+    
     swap(&arr.intervals[i + 1], &arr.intervals[high]);
     return (i + 1);
 }
@@ -118,68 +122,123 @@ void quickSort(IntArray arr, int low, int high) {
         quickSort(arr, pi + 1, high);
     }
 }
- 
-// [0,5],[1,2],[1,3],[3,4],[3,5],[3,9],[3,10],[8,11]
-// 0 -> 1
-// 1 -> 2
-// 3 -> 4
-// 8 -> 1
- 
+
 void swap(Interval* a, Interval* b) {
     Interval t = *a;
     *a = *b;
     *b = t;
 }
- 
-int getMinColor(int* arr, int n) {
-    int min = arr[0];
- 
-    for(int i = 1; i <= n && !arr[i]; i++) {
-        if(arr[i] < min)
-            min = arr[i];
-    }
- 
-    return min;
-}
- 
+
 void InitializeGraph(IntArray* a) {
-    int currChrome = 1;
-    int minColor = 1;
-    int* colorArr = (int*) calloc(a->n, sizeof(int)); // color array of V
- 
     for(int i = 0; i < a->n; i++) {
         for(int j = i + 1; j < a->n; j++) {
             if(a->intervals[i].end >= a->intervals[j].begin) {
                 a->edges++;
-                currChrome++;
- 
-                if(++a->intervals[i].deg < a->minDeg)
+                
+                if(a->minDeg == -1)
+                    a->minDeg = ++a->intervals[i].deg;
+
+                if(a->intervals[i].deg < a->minDeg)
                     a->minDeg = a->intervals[i].deg;
- 
+
                 ++a->intervals[j].deg;
- 
-                if(a->intervals[j].color != -1)
-                    colorArr[a->intervals[j].color-1]++;
- 
-                printf("EDGE: [%d,%d] -> [%d,%d]\n", a->intervals[i].begin, a->intervals[i].end, a->intervals[j].begin, a->intervals[j].end);
             }
         }
- 
-        a->intervals[i].color = getMinColor(colorArr, a->n)+1;
-        for(int i = 0; i < a->n; i++) { colorArr[i] = 0; }
- 
-        if(currChrome > a->chromeNum)
-            a->chromeNum = currChrome;
- 
-        if(a->maxDeg < a->intervals[i].deg)
-            a->maxDeg = a->intervals[i].deg;
- 
-        currChrome = 0;
     }
  
     a->compEdges = (a->n*(a->n-1)/2) - a->edges;
     a->maxCompDeg = a->n-1 - a->minDeg;
     a->minCompDeg = a->n-1 - a->maxDeg;
- 
-    free(colorArr);
+}
+
+void optionalColoring(IntArray* a) {
+      a->intervals[0].color = 0;
+
+      int ready[a->n];
+      int maxConnections = 0;
+      for (int cr = 0; cr < a->n; cr++)
+          ready[cr] = 0;
+
+      for (int u = 1; u < a->n; u++)
+      {
+          for (int i = 0; i < a->n; i++) {
+            if(a->intervals[u].end >= a->intervals[i].begin && a->intervals[i].end >= a->intervals[u].begin) {
+              if (a->intervals[i].color != -1)
+                  ready[a->intervals[i].color] = 1;
+                  
+              if(a->intervals[u].begin != a->intervals[i].begin && a->intervals[u].end != a->intervals[i].end)
+                maxConnections++;
+
+              if(a->maxDeg < maxConnections)
+                  a->maxDeg = maxConnections;
+            }
+          }
+
+          maxConnections = 0;
+          
+          int cr;
+          for (cr = 0; cr < a->n; cr++)
+              if (!ready[cr])
+                  break;
+    
+          a->intervals[u].color = cr;
+          
+          if(cr > a->chromeNum)
+            a->chromeNum = cr;
+
+          for (int i = 0; i < a->n; i++)
+            if(a->intervals[u].end >= a->intervals[i].begin)
+                if (a->intervals[i].color != -1)
+                    ready[a->intervals[i].color] = 0;
+      }
+
+      if(a->chromeNum != 0)
+        a->chromeNum++;
+      else
+        a->chromeNum = 1;
+        
+      if(a->minDeg == -1) a->minDeg = 0;
+}
+
+void printColors(IntArray* a) {
+    printf("Optional Coloring: ");
+    
+    if(a->chromeNum > 0) {
+        int colorArr[a->chromeNum];
+        int currSize = 0;
+
+        for(int i = 0; i < a->chromeNum; i++)
+            colorArr[i] = 0;
+
+        for(int i = 0; i < a->n; i++)
+            colorArr[a->intervals[i].color]++;
+
+        for(int i = 0; i < a->chromeNum; i++) {
+            printf("{");
+            
+            for(int j = 0; j < a->n; j++) {
+                if(a->intervals[j].color == i) {
+                    currSize++;
+                    printf("[%d, %d]", a->intervals[j].begin, a->intervals[j].end);
+                    if(currSize < colorArr[i]) printf(",");
+                }
+            }
+
+            currSize = 0;
+            printf("} = %d", i+1);
+            if(i+1 < a->chromeNum) printf(", ");
+        }
+    }
+    else {
+        printf("{");
+        
+        for(int j = 0; j < a->n; j++) {
+            printf("[%d, %d]", a->intervals[j].begin, a->intervals[j].end);
+            if(j+1 < a->n) printf(",");
+        }
+
+        printf("} = 0");
+    }
+    
+    printf("\n");
 }
